7ecbeb0efe0f925afabb0115f702a155ad5cc92a
Paul-Lukasiewicz <168724120+Paul-Lukasiewicz@users.noreply.github.com>
2024-09-07 14:47:30 +0200

    test

--- scrapp_soce.py
+++ scrapp_soce.py
@@ -30,6 +30,8 @@ def init_and_connect(usr, pswd, spec_metier):
     options.add_argument("enable-automation")
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    
 
     driver = webdriver.Chrome(options=options)
 
