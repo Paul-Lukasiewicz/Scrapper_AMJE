5bb17b7711170da54f5cd719615685350519eaa3
Paul-Lukasiewicz <168724120+Paul-Lukasiewicz@users.noreply.github.com>
2024-09-07 14:59:16 +0200

    fez

--- scrapp_soce.py
+++ scrapp_soce.py
@@ -25,11 +25,13 @@ def init_and_connect(usr, pswd, spec_metier):
     options.add_argument("--headless")
     options.add_argument("window-size=1400,1500")
     options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("start-maximized")
     options.add_argument("enable-automation")
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--no-sandbox')  # Désactiver le sandboxing
+    options.add_argument('--disable-extensions')  # Désactiver les extensions pour réduire la charge
+    options.add_argument('--single-process')  # Exécuter Chrome en un seul processus
+    options.add_argument('--start-maximized')
 
     driver = webdriver.Chrome(options=options)
 
